a310dbb0bb5f03f29d7426ea7cc721279d67eacc
_TheMet_ <44820844+iTheMet@users.noreply.github.com>
2022-05-22 01:36:21 +0300

    шифрование smtp данных

--- TimetableAPI/Services/SMTPConfig.cs
+++ TimetableAPI/Services/SMTPConfig.cs
@@ -1,12 +1,47 @@
-﻿namespace TimetableAPI.Services
+﻿using System.Text;
+
+namespace TimetableAPI.Services
 {
     public class SMTPConfig
     {
         public const string SectionName = "SMTPConfig";
 
-        public string EmailAdress { get; set; }
+        private string emailAdress;
 
-        public string Password { get; set; }
+        public string EmailAdress {
+            get
+            {
+                return emailAdress;
+            }
+            set
+            {
+                string[] outArray = value.Split(',');
+                byte[] outByte = new byte[outArray.Length-1];
+                for (int i = 0; i < outArray.Length - 1; i++)
+                {
+                    outByte[i] = Convert.ToByte(outArray[i]);
+                }
+                emailAdress = Encoding.Default.GetString(outByte);
+            }
+        }
+
+        private string password;
+
+        public string Password { 
+            get 
+            {
+                return password;
+            } 
+            set
+            {
+                string[] outArray = value.Split(',');
+                byte[] outByte = new byte[outArray.Length-1];
+                for (int i = 0; i < outArray.Length - 1; i++)
+                {
+                    outByte[i] = Convert.ToByte(outArray[i]);
+                }
+                password = Encoding.Default.GetString(outByte);
+            } }
 
         public string Host { get; set; }
 
